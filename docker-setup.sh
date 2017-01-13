17843170bf22825a7ed0eb47b79e6eed043bffc0
Shi-Hai Han <shi-hai.han@hpe.com>
2017-01-13 02:15:11 -0500

    change file permission

--- docker-setup.sh
+++ docker-setup.sh
@@ -6,7 +6,7 @@ wget http://apache.mirrors.ionfish.org//jmeter/binaries/apache-jmeter-3.1.tgz
 
 tar -xzf apache-jmeter-3.1.tgz
 
-tm -rf apache-jmeter-3.1.tgz
+rm -rf apache-jmeter-3.1.tgz
 
 # Set Jmeter Home
 export JMETER_HOME="/opt/apache-jmeter-3.1/"
